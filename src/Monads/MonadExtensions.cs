07400e9426b7079d60e882c5da36febef42adfba
Charles Farris <charlesfarris71@gmail.com>
2024-12-29 14:32:34 -0700

    Add tests for MonadExtensions
    - Add extension method

--- src/Monads/MonadExtensions.cs
+++ src/Monads/MonadExtensions.cs
@@ -1,9 +1,6 @@
 ﻿using System.Diagnostics.CodeAnalysis;
 using SleepingBear.Functional.Errors;
 
-// ReSharper disable UnusedType.Global
-// ReSharper disable UnusedMember.Global
-
 namespace SleepingBear.Functional.Monads;
 
 /// <summary>
@@ -14,10 +11,10 @@ public static class MonadExtensions
     /// <summary>
     /// Converts an <see cref="Option{T}"/> to a <see cref="Result{T}"/>.
     /// </summary>
-    /// <param name="option"></param>
-    /// <param name="errorFunc"></param>
-    /// <typeparam name="T"></typeparam>
-    /// <returns></returns>
+    /// <param name="option">The <see cref="Option{T}"/> being converted.</param>
+    /// <param name="errorFunc">The error function.</param>
+    /// <typeparam name="T">The type of the lifted value.</typeparam>
+    /// <returns>A <see cref="Result{T}"/>.</returns>
     [SuppressMessage(category: "ReSharper", checkId: "NullableWarningSuppressionIsUsed")]
     public static Result<T> ToResult<T>(this Option<T> option, Func<Error> errorFunc) where T : notnull
     {
@@ -32,10 +29,10 @@ public static class MonadExtensions
     /// <summary>
     /// Converts an <see cref="Option{T}"/> to a <see cref="Result{T}"/>.
     /// </summary>
-    /// <param name="option"></param>
-    /// <param name="error"></param>
-    /// <typeparam name="T"></typeparam>
-    /// <returns></returns>
+    /// <param name="option">The <see cref="Option{T}"/> being converted.</param>
+    /// <param name="error">The error.</param>
+    /// <typeparam name="T">The type of the lifted value.</typeparam>
+    /// <returns>A <see cref="Result{T}"/>.</returns>
     [SuppressMessage(category: "ReSharper", checkId: "NullableWarningSuppressionIsUsed")]
     public static Result<T> ToResult<T>(this Option<T> option, Error error) where T : notnull
     {
@@ -45,5 +42,20 @@ public static class MonadExtensions
         return isSome
             ? new Result<T>(some!)
             : new Result<T>(error);
+    }
+
+    /// <summary>
+    /// Converts a <see cref="Result{T}"/> to a <see cref="Option{T}"/>.
+    /// </summary>
+    /// <param name="result">The <see cref="Result{T}"/> being converted.</param>
+    /// <typeparam name="T">The type of the lifted value.</typeparam>
+    /// <returns>A <see cref="Option{T}"/>.</returns>
+    [SuppressMessage(category: "ReSharper", checkId: "NullableWarningSuppressionIsUsed")]
+    public static Option<T> ToOption<T>(this Result<T> result) where T : notnull
+    {
+        var (isOk, ok, _) = result;
+        return isOk
+            ? new Option<T>(ok!)
+            : Option<T>.None;
     }
 }

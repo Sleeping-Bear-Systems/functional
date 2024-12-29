52fe52ea286b35f79fb2878ba652482b0d7cdf06
Charles Farris <charlesfarris71@gmail.com>
2024-12-29 00:07:11 -0700

    Reformat code

--- src/Common/PipeExtensions.cs
+++ src/Common/PipeExtensions.cs
@@ -19,10 +19,10 @@ public static class PipeExtensions
     public static TOut Pipe<TIn, TOut>(this TIn value, Func<TIn, TOut> func)
     {
         ArgumentNullException.ThrowIfNull(func);
-        
+
         return func(value);
     }
-    
+
     /// <summary>
     /// Pipes a value asynchronously.
     /// </summary>
@@ -39,7 +39,7 @@ public static class PipeExtensions
         var value = await task.ConfigureAwait(false);
         return func(value);
     }
-    
+
     /// <summary>
     /// Pipes a value asynchronously.
     /// </summary>
@@ -57,4 +57,52 @@ public static class PipeExtensions
         return await func(value).ConfigureAwait(false);
     }
 
+    /// <summary>
+    /// Taps a value.
+    /// </summary>
+    /// <param name="value"></param>
+    /// <param name="action"></param>
+    /// <typeparam name="TIn"></typeparam>
+    /// <returns></returns>
+    public static TIn Tap<TIn>(this TIn value, Action<TIn> action)
+    {
+        ArgumentNullException.ThrowIfNull(action);
+
+        action(value);
+        return value;
+    }
+
+    /// <summary>
+    /// Taps a value asynchronously.
+    /// </summary>
+    /// <param name="task"></param>
+    /// <param name="action"></param>
+    /// <typeparam name="TIn"></typeparam>
+    /// <returns></returns>
+    public static async Task<TIn> TapAsync<TIn>(this Task<TIn> task, Action<TIn> action)
+    {
+        ArgumentNullException.ThrowIfNull(task);
+        ArgumentNullException.ThrowIfNull(action);
+
+        var value = await task.ConfigureAwait(false);
+        action(value);
+        return value;
+    }
+
+    /// <summary>
+    /// Taps a value asynchronously.
+    /// </summary>
+    /// <param name="task"></param>
+    /// <param name="func"></param>
+    /// <typeparam name="TIn"></typeparam>
+    /// <returns></returns>
+    public static async Task<TIn> TapAsync<TIn>(this Task<TIn> task, Func<TIn, Task> func)
+    {
+        ArgumentNullException.ThrowIfNull(task);
+        ArgumentNullException.ThrowIfNull(func);
+
+        var value = await task.ConfigureAwait(false);
+        await func(value).ConfigureAwait(false);
+        return value;
+    }
 }

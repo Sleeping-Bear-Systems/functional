118bc2a5db37dc2e4fb1789273f1448e4cbf9cae
Charles Farris <charlesfarris71@gmail.com>
2025-01-01 12:27:15 -0700

    Add validation  methods

---
+++ src/Validation.Tests/GuidExtensionsTests.cs
@@ -0,0 +1,40 @@
+﻿using SleepingBear.Functional.Errors;
+using SleepingBear.Functional.Testing;
+
+namespace SleepingBear.Functional.Validation.Tests;
+
+/// <summary>
+///     Tests for <see cref="GuidExtensions" />.
+/// </summary>
+internal static class GuidExtensionsTests
+{
+    [Test]
+    public static void ToOptionIsNotEmpty_Empty_ReturnsError()
+    {
+        var option = Guid.Empty.ToOptionIsNotEmpty();
+        TestOption.IsNone(option);
+    }
+
+    [Test]
+    public static void ToOptionIsNotEmpty_NotEmpty_ReturnsOk()
+    {
+        var value = new Guid(g: "37378EF9-4411-4F04-BF54-291CA21BC85B");
+        var option = value.ToOptionIsNotEmpty();
+        TestOption.IsSomeEqualTo(option, value);
+    }
+
+    [Test]
+    public static void ToResultIsNotEmpty_Empty_ReturnsError()
+    {
+        var result = Guid.Empty.ToResultIsNotEmpty(UnknownError.Value);
+        TestResult.IsError<Guid, UnknownError>(result);
+    }
+
+    [Test]
+    public static void ToResultIsNotEmpty_NotEmpty_ReturnsOk()
+    {
+        var value = new Guid(g: "37378EF9-4411-4F04-BF54-291CA21BC85B");
+        var result = value.ToResultIsNotEmpty(UnknownError.Value);
+        TestResult.IsOkEqualTo(result, value);
+    }
+}
---
+++ src/Validation/ObjectExtensions.cs
@@ -0,0 +1,23 @@
+﻿using SleepingBear.Functional.Errors;
+using SleepingBear.Functional.Monads;
+
+namespace SleepingBear.Functional.Validation;
+
+/// <summary>
+///     Extension methods for <see cref="object" />.
+/// </summary>
+public static class ObjectExtensions
+{
+    /// <summary>
+    ///     Lifts a nullable value to a <see cref="Result{TOk}" /> if the value is not null.
+    /// </summary>
+    /// <param name="value">The nullable value.</param>
+    /// <param name="error">The error.</param>
+    /// <typeparam name="T">The type of the lifted value.</typeparam>
+    /// <returns>A <see cref="Result{T}" />.</returns>
+    public static Result<T> ToResultIsNotNull<T>(this T? value, Error error)
+        where T : class
+    {
+        return value?.ToResultOk() ?? error.ToResultError<T>();
+    }
+}

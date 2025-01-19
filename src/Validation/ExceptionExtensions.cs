1d22ba086a65d64146c75226df58bc8a11f592d3
Charles Farris <charlesfarris71@gmail.com>
2025-01-19 14:49:38 -0700

    Add generalized try/catch handler
    - Add ExceptionHandler helper methods

--- src/Validation/ExceptionExtensions.cs
+++ src/Validation/ExceptionExtensions.cs
@@ -1,4 +1,5 @@
-﻿using SleepingBear.Functional.Errors;
+﻿using SleepingBear.Functional.Common;
+using SleepingBear.Functional.Errors;
 using SleepingBear.Functional.Monads;
 
 // ReSharper disable UnusedType.Global
@@ -11,6 +12,157 @@ namespace SleepingBear.Functional.Validation;
 /// </summary>
 public static class ExceptionExtensions
 {
+    /// <summary>
+    /// </summary>
+    public static Option<Error> ExceptionHandler<T1>(this Exception ex) where T1 : Exception
+    {
+        return ex switch
+        {
+            T1 t1 => t1.ToExceptionError().ToOption<Error>(),
+            _ => Option<Error>.None
+        };
+    }
+
+    /// <summary>
+    /// </summary>
+    public static Option<Error> ExceptionHandler<T1, T2>(this Exception ex)
+        where T1 : Exception
+        where T2 : Exception
+    {
+        return ex switch
+        {
+            T1 t1 => t1.ToExceptionError().ToOption<Error>(),
+            T2 t2 => t2.ToExceptionError().ToOption<Error>(),
+            _ => Option<Error>.None
+        };
+    }
+
+    /// <summary>
+    /// </summary>
+    public static Option<Error> ExceptionHandler<T1, T2, T3>(this Exception ex)
+        where T1 : Exception
+        where T2 : Exception
+        where T3 : Exception
+    {
+        return ex switch
+        {
+            T1 t1 => t1.ToExceptionError().ToOption<Error>(),
+            T2 t2 => t2.ToExceptionError().ToOption<Error>(),
+            T3 t3 => t3.ToExceptionError().ToOption<Error>(),
+            _ => Option<Error>.None
+        };
+    }
+
+    /// <summary>
+    /// </summary>
+    public static Option<Error> ExceptionHandler<T1, T2, T3, T4>(this Exception ex)
+        where T1 : Exception
+        where T2 : Exception
+        where T3 : Exception
+        where T4 : Exception
+    {
+        return ex switch
+        {
+            T1 t1 => t1.ToExceptionError().ToOption<Error>(),
+            T2 t2 => t2.ToExceptionError().ToOption<Error>(),
+            T3 t3 => t3.ToExceptionError().ToOption<Error>(),
+            T4 t4 => t4.ToExceptionError().ToOption<Error>(),
+            _ => Option<Error>.None
+        };
+    }
+
+    /// <summary>
+    /// </summary>
+    public static Option<Error> ExceptionHandler<T1, T2, T3, T4, T5>(this Exception ex)
+        where T1 : Exception
+        where T2 : Exception
+        where T3 : Exception
+        where T4 : Exception
+        where T5 : Exception
+    {
+        return ex switch
+        {
+            T1 t1 => t1.ToExceptionError().ToOption<Error>(),
+            T2 t2 => t2.ToExceptionError().ToOption<Error>(),
+            T3 t3 => t3.ToExceptionError().ToOption<Error>(),
+            T4 t4 => t4.ToExceptionError().ToOption<Error>(),
+            T5 t5 => t5.ToExceptionError().ToOption<Error>(),
+            _ => Option<Error>.None
+        };
+    }
+
+    /// <summary>
+    /// </summary>
+    public static Option<Error> ExceptionHandler<T1, T2, T3, T4, T5, T6>(this Exception ex)
+        where T1 : Exception
+        where T2 : Exception
+        where T3 : Exception
+        where T4 : Exception
+        where T5 : Exception
+        where T6 : Exception
+    {
+        return ex switch
+        {
+            T1 t1 => t1.ToExceptionError().ToOption<Error>(),
+            T2 t2 => t2.ToExceptionError().ToOption<Error>(),
+            T3 t3 => t3.ToExceptionError().ToOption<Error>(),
+            T4 t4 => t4.ToExceptionError().ToOption<Error>(),
+            T5 t5 => t5.ToExceptionError().ToOption<Error>(),
+            T6 t6 => t6.ToExceptionError().ToOption<Error>(),
+            _ => Option<Error>.None
+        };
+    }
+
+    /// <summary>
+    /// </summary>
+    public static Option<Error> ExceptionHandler<T1, T2, T3, T4, T5, T6, T7>(this Exception ex)
+        where T1 : Exception
+        where T2 : Exception
+        where T3 : Exception
+        where T4 : Exception
+        where T5 : Exception
+        where T6 : Exception
+        where T7 : Exception
+    {
+        return ex switch
+        {
+            T1 t1 => t1.ToExceptionError().ToOption<Error>(),
+            T2 t2 => t2.ToExceptionError().ToOption<Error>(),
+            T3 t3 => t3.ToExceptionError().ToOption<Error>(),
+            T4 t4 => t4.ToExceptionError().ToOption<Error>(),
+            T5 t5 => t5.ToExceptionError().ToOption<Error>(),
+            T6 t6 => t6.ToExceptionError().ToOption<Error>(),
+            T7 t7 => t7.ToExceptionError().ToOption<Error>(),
+            _ => Option<Error>.None
+        };
+    }
+
+    /// <summary>
+    /// </summary>
+    public static Option<Error> ExceptionHandler<T1, T2, T3, T4, T5, T6, T7, T8>(this Exception ex)
+        where T1 : Exception
+        where T2 : Exception
+        where T3 : Exception
+        where T4 : Exception
+        where T5 : Exception
+        where T6 : Exception
+        where T7 : Exception
+        where T8 : Exception
+    {
+        return ex switch
+        {
+            T1 t1 => t1.ToExceptionError().ToOption<Error>(),
+            T2 t2 => t2.ToExceptionError().ToOption<Error>(),
+            T3 t3 => t3.ToExceptionError().ToOption<Error>(),
+            T4 t4 => t4.ToExceptionError().ToOption<Error>(),
+            T5 t5 => t5.ToExceptionError().ToOption<Error>(),
+            T6 t6 => t6.ToExceptionError().ToOption<Error>(),
+            T7 t7 => t7.ToExceptionError().ToOption<Error>(),
+            T8 t8 => t8.ToExceptionError().ToOption<Error>(),
+            _ => Option<Error>.None
+        };
+    }
+
     /// <summary>
     /// </summary>
     public static Result<TOut> TryCatch<TIn, TOut, T1>(this TIn value, Func<TIn, Result<TOut>> func)
@@ -302,6 +454,33 @@ public static class ExceptionExtensions
         catch (T8 ex)
         {
             return ex.ToExceptionError().ToResultError<TOut>();
+        }
+    }
+
+    /// <summary>
+    /// </summary>
+    public static Result<TOut> TryCatch<TIn, TOut>(
+        this TIn value,
+        Func<TIn, Result<TOut>> func,
+        Func<Exception, Option<Error>> handler)
+        where TIn : notnull
+        where TOut : notnull
+    {
+        ArgumentNullException.ThrowIfNull(func);
+        ArgumentNullException.ThrowIfNull(handler);
+        try
+        {
+            return func(value);
+        }
+        catch (Exception ex)
+        {
+            ex.FailFastIfCritical(message: "SleepingBear.Functional.Validation.ExceptionExtensions.TryCatch");
+            if (handler(ex).TrySome(out var some))
+            {
+                return some.ToResultError<TOut>();
+            }
+
+            throw;
         }
     }
 }

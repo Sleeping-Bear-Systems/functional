26aa8ed82b795bbcb4adb3ebf07b65640a18d6ff
Charles Farris <charlesfarris71@gmail.com>
2025-02-08 16:16:48 -0700

    Format code

--- src/Validation/ExceptionExtensions.cs
+++ src/Validation/ExceptionExtensions.cs
@@ -468,7 +468,7 @@ public static class ExceptionExtensions
     {
         ArgumentNullException.ThrowIfNull(func);
         ArgumentNullException.ThrowIfNull(handler);
-        
+
         try
         {
             return func(value);
